59e0f5776f84308639b820f9bfcff82353e4da15
Marcin Zadka <gamboox@gmail.com>
2015-01-17 18:07:22 +0100

    Categories fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_filter :user_admin?, except: [:index, :show]
 
   expose(:categories)
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_filter :product_owner?, only: [:edit, :update, :destroy]
 
   expose(:category)

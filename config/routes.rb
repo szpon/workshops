954a3a73c02b677f5f23ae136af68f891a8060b9
Marcin Zadka <gamboox@gmail.com>
2015-01-17 05:15:39 +0100

    User profile with last reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :categories do
     resources :products do
       resources :reviews
     end
   end
+
+  resources :users, only: :show
 
   root 'categories#index'
 end
